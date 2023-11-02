e2172fc88d6ee3219103cdbe269812ff9f3bebc1
Nurunnisa Fathanah <dzsbnf21d@student.unhas.ac.id>
2023-11-02 19:01:23 +0800

    feat: added button components

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
+'use client'
+
+import HelloButton from '../components/button/HelloButton'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-      <h1>Welcome to My App</h1>
-      <button>Click Here</button>
-    </>
+    <section style={{
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <h1 style={{
+        margin: 0,
+        marginBottom: "12px",
+        fontSize: "24px"
+      }}>Welcome to My App</h1>
+      <HelloButton buttonText={"Tekan Di sini"} buttonText2={"Di sini juga boleh"}/>
+    </section>
   )
 }
